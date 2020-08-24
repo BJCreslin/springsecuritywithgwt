6c0554f8c7f31894478e83117c1e53c7618e7e82
bjcreslin <bjcreslin@gmail.com>
2020-08-24 17:27:21 +0700

    Added AuthRestController

--- src/main/java/ru/bjcreslin/springsecurity/rest/AuthRestController.java
+++ src/main/java/ru/bjcreslin/springsecurity/rest/AuthRestController.java
@@ -1,23 +1,46 @@
 package ru.bjcreslin.springsecurity.rest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.bjcreslin.springsecurity.model.User;
+import ru.bjcreslin.springsecurity.repository.UserRepository;
+import ru.bjcreslin.springsecurity.security.JwtAuthenticationException;
+import ru.bjcreslin.springsecurity.security.JwtTokenProvider;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthRestController {
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/login")
     public ResponseEntity<?> authentication(@RequestBody AuthenticationRequestDTO authRequest) {
-
-        return null;
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
+            User user = userRepository.findByEmail(authRequest.getEmail()).
+                    orElseThrow(() -> new UsernameNotFoundException("User does`nt exist"));
+            String token = jwtTokenProvider.createToken(authRequest.getEmail(), user.getRole().name());
+            Map<Object, Object> response = new HashMap<>();
+            response.put("email", authRequest.getEmail());
+            response.put("token", token);
+            return ResponseEntity.ok(response);
+        } catch (JwtAuthenticationException e) {
+            return new ResponseEntity<>("Invalid email/status combination", HttpStatus.FORBIDDEN);
+        }
     }
 
     @PostMapping("/logout")
